59fc31caaadbe772270588fced37f87ebfeefec2
Sugar <topevol33@gmail.com>
2024-08-23 17:59:05 +0300

    Color detection(yellow)

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 import cv2
+from PIL import Image
+from OpenCV_color_detection.utils import get_limits
 
 
+color_yellow = [0, 255, 255] #BGR colorspace yellow
 cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
+
+    hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lowerLimit, upperLimit = get_limits(color=color_yellow)
+
+    mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
+
+    mask_ = Image.fromarray(mask)
+
+    bbox = mask_.getbbox()
+
+    if bbox is not None:
+        x1, y1, x2, y2 = bbox
+
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
 
     cv2.imshow('frame', frame)
 
